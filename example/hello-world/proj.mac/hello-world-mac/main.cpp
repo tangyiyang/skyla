4a213d2d4a7dc25a8e47b8f555dfd2d34ec8456b
sleepers.tang <sleepers.tang@gmail.com>
2017-10-04 22:25:26 +0800

    move dir of glfw lib

---
+++ example/hello-world/proj.mac/hello-world-mac/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  hello-world-mac
+//
+//  Created by yiyang.tang on 2017/10/4.
+//  Copyright © 2017年 yiyang.tang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
